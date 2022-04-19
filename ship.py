9909854761d669169baffd9806cf8f113d360353
Jasvaran Mann <mannjesse355@gmail.com>
2022-04-18 19:24:40 -0700

    Added continous movement to right

--- ship.py
+++ ship.py
@@ -8,6 +8,9 @@ class Ship:
 		self.screen = ai_game.screen
 		self.screen_rect = ai_game.screen.get_rect()
 
+		# Movement flag
+		self.moving_right = False
+
 
 		# Load the ship image and get its rect.
 		self.image = pygame.image.load('images/ship.bmp')
@@ -15,6 +18,12 @@ class Ship:
 
 		# Start each new ship at the bottom center of your screen
 		self.rect.midbottom = self.screen_rect.midbottom
+
+	def update(self):
+		"""Update the ships position based on the movement flag"""
+		if self.moving_right:
+			self.rect.x += 1	
+
 
 	def blitme(self):
 		"""Draw the ship at its current location"""
