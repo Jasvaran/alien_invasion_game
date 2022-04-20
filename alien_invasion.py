0eca2db164f1da79078795b8b693f9b994b6d06c
Jasvaran Mann <mannjesse355@gmail.com>
2022-04-20 16:59:26 -0700

    calculated space for aliens and rows and created aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 
 class AlienInvasion:
@@ -19,8 +20,9 @@ class AlienInvasion:
 
 		self.ship = Ship(self)
 		self.bullets = pygame.sprite.Group()
-		
+		self.aliens = pygame.sprite.Group()
 
+		self._create_fleet()
 
 	def run_game(self):
 		"""Start the main loop for the game."""
@@ -78,6 +80,33 @@ class AlienInvasion:
 			if bullet.rect.bottom <= 0:
 				self.bullets.remove(bullet)
 
+	def _create_fleet(self):
+		"""Create a fleet of aliens"""
+		alien = Alien(self)
+		alien_width, alien_height = alien.rect.size
+		available_space_x = self.settings.screen_width - (2 * alien_width)
+		number_aliens_x = available_space_x // (2 * alien_width)
+
+		# Determine the number of rows of aliens that fit on the screen.
+		ship_height = self.ship.rect.height
+		available_space_y = (self.settings.screen_height - (3 * alien_height) - ship_height)
+		number_rows = available_space_y // (2 * alien_height)
+
+		# Create the first row of aliens
+		for row_number in range(number_rows):
+			for alien_number in range(number_aliens_x):
+				self._create_alien(alien_number, row_number)
+
+
+	def _create_alien(self, alien_number, row_number):
+		"""Create an alien and place it in the row"""
+		alien = Alien(self)
+		alien_width, alien_height = alien.rect.size
+		alien.x = alien_width + 2 * alien_width * alien_number
+		alien.rect.x = alien.x
+		alien.rect.y = alien_height + 2 * alien.rect.height * row_number
+		self.aliens.add(alien)
+
 
 	def _update_screen(self):
 		"""Update Images on the screen and flip to the new screen"""
@@ -85,6 +114,9 @@ class AlienInvasion:
 		self.ship.blitme()
 		for bullet in self.bullets.sprites():
 			bullet.draw_bullet()
+		self.aliens.draw(self.screen)
+		
+
 		# Make the most recently drawn screen visible.
 		pygame.display.flip()
 
