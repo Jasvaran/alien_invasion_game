d04fcb19c62ef193487ab994087cec9c69393b76
Jasvaran Mann <mannjesse355@gmail.com>
2022-04-20 21:38:00 -0700

    added movement to aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -30,6 +30,7 @@ class AlienInvasion:
 			self._check_events()
 			self.ship.update()
 			self._update_bullets()
+			self._update_aliens()
 			self._update_screen()
 
 
@@ -89,7 +90,8 @@ class AlienInvasion:
 
 		# Determine the number of rows of aliens that fit on the screen.
 		ship_height = self.ship.rect.height
-		available_space_y = (self.settings.screen_height - (3 * alien_height) - ship_height)
+		available_space_y = (self.settings.screen_height - 
+								(3 * alien_height) - ship_height)
 		number_rows = available_space_y // (2 * alien_height)
 
 		# Create the first row of aliens
@@ -106,6 +108,26 @@ class AlienInvasion:
 		alien.rect.x = alien.x
 		alien.rect.y = alien_height + 2 * alien.rect.height * row_number
 		self.aliens.add(alien)
+
+	def _check_fleet_edges(self):
+		"""Respond appropriately if any aliens have reached the edge"""
+		for alien in self.aliens.sprites():
+			if alien.check_edges():
+				self._change_fleet_direction()
+				break
+
+	def _change_fleet_direction(self):
+		"""Drop the entire fleet and change the fleets direction"""
+		for alien in self.aliens.sprites():
+			alien.rect.y += self.settings.fleet_drop_speed
+		self.settings.fleet_direction *= -1
+
+	def _update_aliens(self):
+		"""Update the positions of all aliens in the fleet
+			then update the positions of all aliens in the fleet.
+		"""
+		self._check_fleet_edges()
+		self.aliens.update()
 
 
 	def _update_screen(self):
